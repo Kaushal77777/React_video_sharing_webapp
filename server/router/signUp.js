3745261fb6d844d57e3f1b63c86d9cff02e0ba2c
Kaushal Chovatiya <patelkaushal736@gmail.com>
2021-09-26 23:53:13 +0530

    adding some comments in signup router

--- server/router/signUp.js
+++ server/router/signUp.js
@@ -2,31 +2,29 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-
 const User = require('../models/User');
 
-// hash is not reversible
-// The idea behind salting is that user may enter common password
-// like icecream, etc. which can be present in attacker's dictionary.
-// So, salting adds some random string to icecream before we hash it.
-// 10 is number of salting rounds
+
+
 router.post('/', (req, res, next) => {
   console.log(req.body);
-  // 409 means conflict
-  // 422 means unprocess about entity
+  
+  
   User.find({ email: req.body.email })
     .exec()
-    .then(user => {
-      // By default, if no user found, user = []
+    .then(user => {                                             // By default, if no user found, user = []
       if (user.length >= 1) {
-        return res.status(409).json({
+        return res.status(409).json({                           // 409 means conflict
           message: 'Mail exists'
         });
       } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {     // hash is not reversible
+                                                                // The idea behind salting is that user may enter common password
+                                                                // So, salting adds some random string to icecream before we hash it.
+                                                                // 10 is number of salting rounds
           if (err) {
             console.log(err);
-            return res.status(500).json({
+            return res.status(500).json({                       //500 means internal server error
               error: err
             });
           } else {
@@ -41,13 +39,13 @@ router.post('/', (req, res, next) => {
               .save()
               .then(result => {
                 console.log(result);
-                res.status(201).json({
+                res.status(201).json({                          //201 means new resource have been added
                   message: 'User created'
                 });
               })
               .catch(err => {
                 console.log(err);
-                res.status(500).json({
+                res.status(500).json({                          //500 means internal server error
                   error: err
                 });
               });
@@ -57,7 +55,7 @@ router.post('/', (req, res, next) => {
     })
     .catch(err => {
       console.log(err);
-      res.status(422).json({
+      res.status(422).json({                                    // 422 means unprocess about entity
         error: err
       });
     });
