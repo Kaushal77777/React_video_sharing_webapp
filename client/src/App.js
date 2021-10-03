550f074a6dc9dd95740e136af3728528432a80d5
Kaushal77777 <patelkaushal736@gmail.com>
2021-10-03 20:23:39 +0530

    Creating frontend for signin and signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import SignIn from './components/Form/SignIn/SignIn.js';
+import SignUp from './components/Form/SignUp/SignUp.js'
 
 function App() {
   return (
-    <div className="App">
-      <hi>React app is running ...</hi>
-    </div>
+    <React.Fragment>
+      <Route exact path="/signIn" component={SignIn}/>
+      <Route exact path="/signUp" component={SignUp}/>
+    </React.Fragment>
+    
   );
 }
 
